964d09fb349ce3d70b894d4a46deaed9dcba681b
varun-d-0002 <varund0002@gmail.com>
2023-12-14 12:53:23 +0530

    basic

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3001
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('BYE World! this is not listening to anything....boring....')
 })
 
 app.listen(port, () => {
